2b04371667b64aaeb7583b4a08d7894c24bd931d
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-03-27 15:13:55 -0600

    Making the header  and given styles. Also getData from the API and load to the app

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const API_URL = "https://yts.mx/api/v2/list_movies.json?genre"
+const container = document.getElementById('root');
+
+// Adding interactivity to the icons on the header section
+
+const $iconHamburger = document.querySelector('.icon-hamburger');
+const $iconClose = document.querySelector('.icon-close-menu');
+
+$iconHamburger.addEventListener('click', showModal);
+$iconClose.addEventListener('click', hideModal);
+
+function showModal(ev) {
+    $iconHamburger.style.display = 'none';
+    $iconClose.style.display = 'block';
+}
+
+function hideModal(ev) {
+    $iconHamburger.style.display = 'block';
+    $iconClose.style.display = 'none';
+}
+
+// Geting values of the API
+const getData = async (url) => {
+    const response = await fetch(url);
+    const { data } = await response.json();
+    return data.movies;    
+};
+
+const movieTemplate = (peli) => {
+    return (`
+        <article class="article__movie">
+            <figure class="article__image">
+                <img src=${peli.medium_cover_image} alt="Image of the movie">
+            </figure>
+            <div class="text__container">
+                <h1 class="article__title">${peli.title}</h1>
+                <em>$1 USD</em>
+                <p class="article__description"><b>Synopsis:</b> ${peli.synopsis}</p>
+                <div class="buttons__container">
+                    <button class="article__button--rent">Rent</button>
+                    <button class="article__button--buy">Buy</button> 
+                </div>                
+            </div>
+        </article>
+    `)
+}
+
+async function load() {
+    const datos = await getData(API_URL);
+    console.log(datos);
+    datos.forEach(movie => {
+        const HTMLString = movieTemplate(movie);
+        const html = document.implementation.createHTMLDocument()
+        html.body.innerHTML = HTMLString;
+        container.append(html.body.children[0]);
+    })
+}
+load();
